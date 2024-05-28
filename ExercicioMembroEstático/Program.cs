418239b002df268ea8a9a561a66e540e4de24e33
Guilherme Souza Mercedes <guilhermesouzamercedes@gmail.com>
2024-05-28 17:33:34 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioMembroEstático/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioMembroEstático
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
